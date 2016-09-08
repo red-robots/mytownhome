b7357522b976f4956d8dd2c1763d9cd931bc6f37
Austin Crane <austincrane@gmail.com>
2016-09-08 09:33:52 -0400

    move yoast

--- functions.php
+++ functions.php
@@ -370,3 +370,11 @@ function build_taxonomies() {
 	) );
 	
 } // End build taxonomies
+
+ /*-------------------------------------
+  Move Yoast to the Bottom
+---------------------------------------*/
+function yoasttobottom() {
+  return 'low';
+}
+add_filter( 'wpseo_metabox_prio', 'yoasttobottom');

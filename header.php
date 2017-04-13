49ed020bd2e6da03ba8dba8dd946d03e5502db0f
Austin Crane <austincrane@gmail.com>
2017-04-13 15:26:19 -0400

    new recaptcha

--- header.php
+++ header.php
@@ -83,7 +83,24 @@ else { document.getElementById(d).style.display = "none"; }
 // --> 
 </script>
 
- <script src='https://www.google.com/recaptcha/api.js'></script>
+<script src="https://www.google.com/recaptcha/api.js?onload=myCallBack&render=explicit" async defer></script>
+<script>
+      var recaptcha1;
+      var recaptcha2;
+      var myCallBack = function() {
+        //Render the recaptcha1 on the element with ID "recaptcha1"
+        recaptcha1 = grecaptcha.render('recaptcha1', {
+          'sitekey' : '6Lc_0f4SAAAAAF9ZA', //Replace this with your Site key
+          'theme' : 'light'
+        });
+        
+        //Render the recaptcha2 on the element with ID "recaptcha2"
+        recaptcha2 = grecaptcha.render('recaptcha2', {
+          'sitekey' : '6Lc_0f4SAAAAAF9ZA', //Replace this with your Site key
+          'theme' : 'dark'
+        });
+      };
+    </script>
 
 
 <?php the_field('google_analytics', 'option'); ?>
